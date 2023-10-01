754f78d2c00924130cd36263b239e004966354eb
lebin25 <lebin642@gmail.com>
2023-10-02 03:38:52 +0700

    updates products url

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -18,7 +18,7 @@ const ProductCard = (props) => {
          <div
             className={`${location.pathname == "/product" ? `gr-${grid}` : "col-3"} `}
          >
-            <Link to=':id' className="product-card position-relative">
+            <Link to={`${location.pathname == "/" ? '/product/:id' : location.pathname == '/product/:id' ? "/product/:id" : ":id"}`} className="product-card position-relative">
                <div className="wishlist-icon position-absolute">
                   <button className='border-0 bg-trasparent'>
                      <img src={wish} alt="wishlist" />
@@ -63,7 +63,7 @@ const ProductCard = (props) => {
          <div
             className={`${location.pathname == "/product" ? `gr-${grid}` : "col-3"} `}
          >
-            <Link to=':id' className="product-card position-relative">
+            <Link to={`${location.pathname == "/" ? '/product/:id' : location.pathname == '/product/:id' ? "/product/:id" : ":id"}`} className="product-card position-relative">
                <div className="wishlist-icon position-absolute">
                   <button className='border-0 bg-trasparent'>
                      <img src={wish} alt="wishlist" />
