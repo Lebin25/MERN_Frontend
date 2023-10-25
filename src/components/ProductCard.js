73b731fd1bbaf1c01c140e871f4a5820a7cbd1d4
lebin25 <lebin642@gmail.com>
2023-10-26 00:58:55 +0700

    Features/Blog Done

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -44,7 +44,7 @@ const ProductCard = (props) => {
                            </button>
                         </div>
                         <div className="product-image">
-                           <img src={item?.images[0].url} className='img-fluid mx-auto' alt="product image" width={160} />
+                           <img src={item?.images[0]?.url} className='img-fluid mx-auto' alt="product image" width={160} />
                            <img src={watch2} className='img-fluid mx-auto' alt="product image" width={160} />
                         </div>
                         <div className="product-details">
