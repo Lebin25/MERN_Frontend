4ff8f9da9a31e263a33bbdb0de3ec5612d05739c
lebin25 <lebin642@gmail.com>
2023-11-09 15:36:21 +0700

    Filter Product

--- src/features/products/productService.js
+++ src/features/products/productService.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 import { base_url, config } from '../../utils/axiosconfig'
 
-const getProducts = async () => {
-   const response = await axios.get(`${base_url}product?page=1&limit=30`)
+const getProducts = async (data) => {
+   console.log(data);
+   const response = await axios.get(
+      `${base_url}product?page=1&limit=30&${data?.brand ? `brand=${data.brand}&` : ""}${data?.tag ? `tags=${data.tag}&` : ""}${data?.category ? `category=${data.category}&` : ""}${data?.minPrice ? `price[gte]=${data.minPrice}&` : ""}${data?.maxPrice ? `price[lte]=${data.maxPrice}&` : ""}${data?.sort ? `sort=${data.sort}&` : ""}`
+   )
    if (response.data) {
       return response.data
    }
